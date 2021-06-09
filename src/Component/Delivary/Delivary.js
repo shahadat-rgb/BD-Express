2b1c7c67aa82e63cefc4a7b85062c06eaae4e8e9
md shahadat hossain <mdshahadathossain10200@gmail.com>
2021-06-09 13:38:43 -0700

    delivary section updated

--- src/Component/Delivary/Delivary.js
+++ src/Component/Delivary/Delivary.js
@@ -9,21 +9,21 @@ const Delivary = () => {
     return (
         <div className='container'>
             <h1 className='text-center' style={{marginTop:"50px",marginBottom:"50px"}}>Our <span className='text-warning'>Delivary</span>Man</h1>
-            <div className="row mb-5">
+            <div className="row mb-5 align-items-center text-center">
                 <div className="col-md-4 delivary">
                      <img style={{height:"300px"}} src={delivary1} alt="" />
                      <h3 className='text-warning'>DM. Najmul haq</h3>
-                     <h5 className="text-success ml-3"> <FontAwesomeIcon icon={faPhoneAlt}> </FontAwesomeIcon>  +88704745454</h5>
+                     <h5 className="text-success"> <FontAwesomeIcon icon={faPhoneAlt}> </FontAwesomeIcon>  +88704745454</h5>
                 </div>
                 <div className="col-md-4 delivary">
                 <img style={{height:"300px"}} src={delivary2} alt="" />
                      <h3 className='text-warning'>DM. Sultan hanif</h3>
-                     <h5 className="text-success ml-3"> <FontAwesomeIcon icon={faPhoneAlt}> </FontAwesomeIcon>  +88798577854</h5>
+                     <h5 className="text-success"> <FontAwesomeIcon icon={faPhoneAlt}> </FontAwesomeIcon>  +88798577854</h5>
                 </div>
                 <div className="col-md-4 delivary">
                 <img style={{height:"300px"}} src={delivary3} alt="" />
-                     <h3 className='text-warning'>DM. Miycel patowary</h3>
-                     <h5 className="text-success ml-3"> <FontAwesomeIcon icon={faPhoneAlt}> </FontAwesomeIcon>  +88458349058</h5>
+                     <h3 className='text-warning'>DM. Miycel piti</h3>
+                     <h5 className="text-success"> <FontAwesomeIcon icon={faPhoneAlt}> </FontAwesomeIcon>  +88458349058</h5>
                 </div>
             </div>
         </div>
